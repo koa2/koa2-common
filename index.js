bc5325fbcb5f32c1cfdb1c6a5c66a931bcf07600
i5ting <i5ting@126.com>
2016-07-10 18:11:18 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const path = require('path')
 const load_koa_middlewares = require('load_koa_middlewares')
 const default_config = path.join(__dirname, './conf.js')
+
 module.exports = (path) => {
   let config = default_config
   
